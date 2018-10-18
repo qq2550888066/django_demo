b63b689dba3fdecc721b1c58ee44d5e94422533a
Mr Chen <chen@chen.com>
2018-10-18 11:39:28 +0800

    请求的几种方式

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,30 @@
 from django.conf.urls import url
 
 #  urlpatterns 是被Django自动识别的路由变量
-from users import views
+from users.views import *
 
 urlpatterns = [
     # 每一个路由变量都需要被 url  函数来构造
     # url(路径, 视图)
-    url(r'^index/$', views.index,name='index'),
-    url(r'^say/$', views.say),
+    url(r'^index/$', index, name='index'),
+    url(r'^say/$', say),
+    # 重定向
+    url(r'^response_view/$', response_view),
+
+    url(r'^weather1/([a-z]+)/(\d{4})/$', weather1),
+    url(r'^weather2/(?P<city>[a-z]+)/(?P<year>\d{4})/$', weather2),
+
+    # 从查询字符串获取数据
+    url(r'^query_params$', query_params),
+
+    # 从请求体获取数据 表单形式
+    url(r'^form$', form),
+    # 从请求体获取数据 表单形式
+    url(r'^json_data$', json_data),
+
+    # 从请求头获取数据
+    url(r'^header$', header),
+
+    # 请求对象的其他属性
+    url(r'^other$', other),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 # from django.urls import reverse
 from django.core.urlresolvers import reverse
+
 
 def index(request):
     """
@@ -20,3 +21,86 @@ def say(request):
     # 跳转到/users/index
     # return redirect(url)
     return HttpResponse("SAY")
+
+
+# 请求的几种方式
+# 1. 按照url路径参数传递
+# 未命名参数按定义顺序传递
+def weather1(request, city, year):
+    print('city=%s' % city)
+    print('year=%s' % year)
+    return HttpResponse('OK')
+
+
+# 命名参数按名字传递
+def weather2(request, year, city):
+    print('city=%s' % city)
+    print('year=%s' % year)
+    return HttpResponse('OK')
+
+
+# 获取查询字符串数据
+def query_params(request):
+    # request.GET 包含查询字符串数据
+    data = request.GET
+    print(data)
+    # 打印单个值
+    print(data.get('a'))
+    print(data.get('b'))
+    # 打印所有的值
+    print(data.getlist('a'))
+    return HttpResponse('OK')
+
+
+# 获取表单数据
+def form(request):
+    # request.POST 包含表单数据
+    data = request.POST
+    print(data)
+    # 打印单个值
+    print(data.get('a'))
+    print(data.get('b'))
+    # 打印所有的值
+    print(data.getlist('a'))
+
+    # 获取查询字符串数据
+    data = request.GET
+    print('查询字符串数据')
+    print(data)
+
+    return HttpResponse('OK')
+
+
+# 获取json
+def json_data(request):
+    # 1. 拿到原始数据 request.body bytes
+    # 2. 转换为字符串 body.decode()
+    # 3. 使用json.loads(body_str) 转化为python内置对象
+    body_bytes = request.body
+    body_str = body_bytes.decode()
+    data = json.loads(body_str)
+    print(data)
+    return HttpResponse('OK')
+
+
+# 请求头
+def header(request):
+    # request.META 包含请求头数据
+    headers = request.META
+    print(headers['CONTENT_LENGTH'])
+    print(headers['CONTENT_TYPE'])
+    return HttpResponse('OK')
+
+
+# 请求对象的其他属性
+def other(request):
+    print('method: ', request.method)
+    print('user: ', request.user)
+    print('path: ', request.path)
+    print('encoding: ', request.encoding)
+    print('files: ', request.FILES)
+    return HttpResponse('OK')
+
+
+def response_view(request):
+    return redirect('/users/index')
